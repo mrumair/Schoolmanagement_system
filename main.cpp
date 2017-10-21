83505086418885c019f09accc351d4326bc8c801
Adnan_Mohib <adnanmuhib786@gmail.com>
2017-10-21 21:21:01 +0500

    Add Comments in the Interface of the project

--- main.cpp
+++ main.cpp
@@ -12,13 +12,13 @@
 #include<conio.h>
 #include<Windows.h>
 #include"school.h"
-
-
 using namespace std;
+
 int main()
 {
-	abc:                                                                       //to show manu again and agin
-	
+	// label for the menu
+	abc:                 
+	// creating object of the School                                                       //to show manu again and agin	
 	school schoolpart;
 	int no;
 	string welcome = "\t***********Welcome to R-School System**********\n";   
@@ -27,32 +27,46 @@ int main()
 		cout << welcome[i];
 		Sleep(30);
 	}
+	// displaying menu items
 	cout << "\n\n\n\tWhich School do you want to visit?\n" << endl;            
 	cout << "\t\t1-Primary \n\t\t2-Middle \n\t\t3-High" << endl;               // show option to slect  a School poration
 	cout << "\tEnter Number : ";
+	// taking input from the user for menu choice
 	cin >> no;
 	cout << endl;
 	switch (no)                                                                // switch statement 
 	{
-	case 1:
-	  {
-		schoolpart.printP();
-		break; 
-	   }
 
-	case 2:
-	   {
-			  schoolpart.printM();
-	   break;
-	   }
-	case 3:
-	   {schoolpart.printH();
-	   break;
-	   }
-	default:
-	    {cout << "Wrong input" << endl;
-	     break;
-	   }
+		case 1: // primary school
+		  {
+		  	// clear the console screen after user enters choice
+		  	System("cls");
+		  	// primary school visiting
+			schoolpart.printP();
+			break; 
+		   }
+
+		case 2: //Middle or Elementry School
+		   {
+		   	// clear the console screen after usre enters choice
+		   	System("cls");
+			schoolpart.printM();
+		    break;
+		   }
+		case 3: // High or Secondary School
+		   {
+		   	// clear the console screen after usre enters choice
+		   	System("cls");
+		   	schoolpart.printH();
+		   	break;
+		   }
+		default: // default case
+		   {
+		   	System("cls");
+		    cout << "Wrong input" << endl;
+		    goto abc;
+		    break;
+		   }
 	}//switch end
 
 	cout << "************************************************************************************" << endl;
